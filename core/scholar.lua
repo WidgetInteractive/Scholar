744821b0353df6cb4e31b428db7377780f9ee25e
Ghost1227 <dgriffiths@section214.com>
2017-05-07 18:11:38 -0500

    Fix typo in carry capacity

--- core/scholar.lua
+++ core/scholar.lua
@@ -1,6 +1,6 @@
 -- Constants -------------------------------------------------------------------
 ADDON_NAME    = "Scholar"
-ADDON_VERSION = "1.2.2"
+ADDON_VERSION = "1.2.3"
 ADDON_WEBSITE = "http://www.esoui.com/downloads/info1670-Scholar.html"
 
 -- Local variables -------------------------------------------------------------
